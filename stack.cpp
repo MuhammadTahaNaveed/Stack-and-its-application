7001414c4c352c3dc0ff233d905f1c8b390c7756
MuhammadTahaNaveed <m.taha.naveed27@gmail.com>
2021-11-14 20:20:52 +0500

    second commit: added infix to postfix converter and postfix evaluator

--- stack.cpp
+++ stack.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 #define maxSize 100
@@ -54,12 +55,182 @@ class Stack
             }
 };
 
+class InfixConverter
+{
+      //declaring instance variables
+      string infix ;
+      string postfix ;
+      int evaluatedOutput;
+      Stack<char> stack;
+
+      /*this function will evaluate the operand1 (operator) operand2
+      type expression depending upon the operator*/
+      double evaluate(int op1,int op2,char opr)
+      {
+            switch (opr)
+            {
+                  case '+':
+                        return double(op1+op2);
+                  case '-':
+                        return double(op1-op2);
+                  case '*':
+                        return double(op1*op2);
+                  case '/':
+                        return double(op1/op2);
+                  default:
+                        return 0;
+            }
+      }
+
+      //returns true if the argument is operator e.g. +,-,*,/
+      bool isOperator(char value)
+      {
+            return value == '*' || value == '/' || value == '+' || value == '-';
+      }
+
+      //this function will convert infix to postfix.
+      string convertToPostfix(string input)
+      {
+            string output = "";
+            //until the input is scanned, for loop runs.
+            for (char const temp : input)
+            {
+                  //if there is a bracket opener, it is pushed into stack.
+                  if (temp=='(')
+                  {
+                        stack.push('(');
+                  }
+                  //if it is bracket closer, then stack will be poped until '('
+                  else if(temp==')')
+                  {
+                        output+=" ";
+                        while(*stack.peek() != '(')
+                        {
+                              output+=*stack.peek();
+                              stack.pop();
+                        }
+                        stack.pop();
+                  }
+                  //if it is an operator, then the following conditions are checked.
+                  else if(isOperator(temp))
+                  {
+                        output+=" ";
+
+                        if (stack.peek() == NULL || (*stack.peek() == '('))
+                              stack.push(temp);
+                        else if((*stack.peek() == '+' || *stack.peek() == '-') && (temp=='*' || temp == '/'))
+                              stack.push(temp);
+                        else
+                        {
+                              while(!(stack.peek() == NULL) )
+                              {
+                                    output += *stack.peek();
+                                    output+=' ';
+                                    stack.pop();
+                              }
+                              stack.push(temp);
+                        }
+
+                  }
+                  else if(temp != ' ')
+                  {
+                        output+=temp;
+                  }
+            }
+            while(!(stack.peek() == NULL))
+            {
+                  output+=" ";
+                  output+= *stack.peek();
+                  stack.pop();
+            }
+            return output;
+      }
+
+      //this function will evaluate the postfix expression.
+      double evaluatePostfix(string input)
+      {
+            Stack<double> evStack;
+            string operand = "";
+            double op1;
+            double op2;
+
+            for (char const val : input)
+            {
+                  if (isOperator(val))
+                  {
+                        op2 = *evStack.peek();
+                        evStack.pop();
+                        op1 = *evStack.peek();
+                        evStack.pop();
+
+                        evStack.push(evaluate(op1,op2,val));
+                  }
+                  else if (val == ' ' && operand != "")
+                  {
+                        evStack.push(stoi(operand));
+                        operand = "";
+                  }
+                  else if (val != ' ')
+                        operand += val;
+            }
+            if (!(evStack.peek() == NULL))
+            {
+                  op2 = *evStack.peek();
+                  evStack.pop();
+            }
+            else if (!(operand == ""))
+                  op2 = stoi(operand);
+
+            return op2;
+      }
+
+      
+      public:
+            //constructor
+            InfixConverter()
+            {
+                  infix = "";
+                  postfix = "";
+                  evaluatedOutput = 0;
+            }
+            //function to feed infix.
+            void feedInfix(string input)
+            {
+                  infix = input;
+                  postfix = convertToPostfix(infix);
+                  evaluatedOutput = evaluatePostfix(postfix);
+            }
+            //getter for postfix
+            string getPostfix()
+            {
+                  return postfix;
+            }
+            //getter for evaluated output
+            double getOutput()
+            {
+                  return evaluatedOutput;
+            }
+            //function to return the instance variables of object.
+            string toString()
+            {
+                  return "\nInfix              = " + infix +
+                         "\nPostfix            = " + postfix +
+                         "\nEvaluated Postfix  = " + to_string(evaluatedOutput); 
+            }
+
+};
+
 int main()
 {
-      Stack <int> stack;
-      stack.push(100);
-      cout << *stack.peek();
-      stack.pop();      
+      InfixConverter tn;
+
+      //test case 1
+      tn.feedInfix("9+9+88*(99*34)+(99-100)*199");
+      cout << tn.toString() << endl;
+
+      //test case 2
+      tn.feedInfix("90*90*9+(99/9)/11+(90/9)+100");
+      cout << tn.toString() << endl;
 
       return 0;
 }
